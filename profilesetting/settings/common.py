3aae80c0578bbb3453178d496179256e4159d8cc
dsanup <anup12.m@gmail.com>
2024-01-13 09:21:50 +0600

    modify settings and database

--- profilesetting/settings/common.py
+++ profilesetting/settings/common.py
@@ -76,9 +76,19 @@ WSGI_APPLICATION = "profilesetting.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "anup",
+        "USER": "root",
+        "PASSWORD": "139254",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+            "charset": "utf8mb4",
+            "use_unicode": True,
+        },
     }
 }
 
