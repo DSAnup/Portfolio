1b61a481de13bc2f6431f18711a24475df543e78
dsanup <anup12.m@gmail.com>
2024-01-15 21:32:45 +0600

    add settings media url & root

--- profilemodule/urls.py
+++ profilemodule/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path("", views.say_hello),
     path("home2/", views.index2),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
