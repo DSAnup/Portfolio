20125263063bdd59c33e53b74d313e25753365a1
dsanup <anup12.m@gmail.com>
2024-01-17 15:27:49 +0600

    update template includes html files

--- profilesetting/urls.py
+++ profilesetting/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("", include("profilemodule.urls")),
     path("admin/", admin.site.urls),
+    path('argon/', include("admin_argon.urls")),
 ]
