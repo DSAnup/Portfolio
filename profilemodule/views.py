b1cfa791df549ab3e561708da625212ca28d40cb
dsanup <anup12.m@gmail.com>
2024-02-15 11:59:27 +0600

    fixed obj not found

--- profilemodule/views.py
+++ profilemodule/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from profilemodule.models import *
 from pprint import pprint
-from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
@@ -10,8 +9,8 @@ def index(request):
     host_name = request.get_host()
     if host_name == 'anupmondal.me':
         localuser = 'anup'
-        user = get_object_or_404(User, username=localuser)
-        about = get_object_or_404(About, created_by = user.id)
+        user = User.objects.get(username=localuser)
+        about = About.objects.filter(created_by = user.id)
         socialplatforms = SocialPlatform.objects.filter(created_by = user.id).order_by('order_number')
         experiences = Experience.objects.filter(created_by = user.id).order_by('order_number')
         educations = Education.objects.filter(created_by = user.id).order_by('order_number')
@@ -29,9 +28,9 @@ def index(request):
         return render(request, "core/index.html", data)
     
     elif host_name == '127.0.0.1:8000':
-        localuser = 'anup'
-        user = get_object_or_404(User, username=localuser)
-        about = get_object_or_404(About, created_by = user.id)
+        localuser = 'pronoy'
+        user = User.objects.get(username=localuser)
+        about = About.objects.filter(created_by = user.id)
         socialplatforms = SocialPlatform.objects.filter(created_by = user.id).order_by('order_number')
         experiences = Experience.objects.filter(created_by = user.id).order_by('order_number')
         educations = Education.objects.filter(created_by = user.id).order_by('order_number')
@@ -46,12 +45,12 @@ def index(request):
             'educations': educations,
             'hostname': host_name,
         }
-        return render(request, "core/index2.html", data)
+        return render(request, "core/index2.html")
     
     elif host_name == 'pronoymondal.me':
         localuser = 'pronoy'
-        user = get_object_or_404(User, username=localuser)
-        about = get_object_or_404(About, created_by = user.id)
+        user = User.objects.get(username=localuser)
+        about = About.objects.filter(created_by = user.id)
         socialplatforms = SocialPlatform.objects.filter(created_by = user.id).order_by('order_number')
         experiences = Experience.objects.filter(created_by = user.id).order_by('order_number')
         educations = Education.objects.filter(created_by = user.id).order_by('order_number')
@@ -70,8 +69,8 @@ def index(request):
     
     else:
         localuser = 'anup'
-        user = get_object_or_404(User, username=localuser)
-        about = get_object_or_404(About, created_by = user.id)
+        user = User.objects.get(username=localuser)
+        about = About.objects.filter(created_by = user.id)
         socialplatforms = SocialPlatform.objects.filter(created_by = user.id).order_by('order_number')
         experiences = Experience.objects.filter(created_by = user.id).order_by('order_number')
         educations = Education.objects.filter(created_by = user.id).order_by('order_number')
